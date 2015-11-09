ecb5671db1b09e535d75da84ff6ef84e1a9626e7
Jarred Capellman <jcapellman@hotmail.com>
2015-11-08 20:39:31 -0500

    Added: Implemented Share VIew

--- jcTRENDNET/Views/MainPage.xaml.cs
+++ jcTRENDNET/Views/MainPage.xaml.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using jcTRENDNET.Objects;
+
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace jcTRENDNET.Views
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+namespace jcTRENDNET.Views {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
 
             DataContext = new Viewmodels.LiveViewModel();
 
             ((Viewmodels.LiveViewModel)DataContext).LoadData();
         }
-    }
 
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectedItem = ((ListView)sender).SelectedItem;
+
+            if (selectedItem == null) {
+                return;
+            }
+
+            Frame.Navigate(typeof(SharePage), (LiveCameraResponseItem)selectedItem);
+        }
+    }
 }
--- jcTRENDNET/Views/SharePage.xaml.cs
+++ jcTRENDNET/Views/SharePage.xaml.cs
@@ -1,13 +1,23 @@
 ﻿using Windows.UI.Xaml.Controls;
 
 using jcTRENDNET.Viewmodels;
+using Windows.UI.Xaml.Navigation;
+using jcTRENDNET.Objects;
 
 namespace jcTRENDNET.Views {
     public sealed partial class SharePage : Page {
+        private ShareModel viewModel => (ShareModel)DataContext;
+
         public SharePage() {
             this.InitializeComponent();
+        }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
             DataContext = new ShareModel();
+
+            viewModel.Camera = (LiveCameraResponseItem)e.Parameter;
+
+            base.OnNavigatedTo(e);
         }
     }
 }
